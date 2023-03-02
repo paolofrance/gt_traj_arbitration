b4fdb0e0ddfcbdd3b193f4fef43b33a81ef3bfdf
marie <franceschi.pl@gmail.com>
2023-03-02 20:11:32 +0100

    p

--- src/gt_traj_arbitration/gt_traj_arbitration.cpp
+++ src/gt_traj_arbitration/gt_traj_arbitration.cpp
@@ -1154,6 +1154,10 @@ bool GTTrajArbitration::doUpdate(const ros::Time& time, const ros::Duration& per
   
   void GTTrajArbitration::forceCheck(const std_msgs::BoolConstPtr& msg)
   {
+    CNR_ERROR(this->logger(),"\n\n\n DANGEROUS ERROR\n\n");
+    CNR_ERROR(this->logger(),"CAMERA IS NOT SEEING ANY PLY . ABORTING \n\n");
+    CNR_ERROR(this->logger(),"RESTART NEEDED \n\n");
+    
     std::abort();
   }
   
